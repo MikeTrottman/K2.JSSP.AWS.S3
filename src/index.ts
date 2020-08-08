ee5cf8558faf8a621b4284304a0fc8ffdcdcd14e
Michael Trottman <mtrottman@k2.com>
2020-08-07 17:09:27 -0700

    AWS S3 configuration initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,103 @@
 import '@k2oss/k2-broker-core';
 
 metadata = {
-    systemName: "com.k2.example",
-    displayName: "Example Broker",
-    description: "An example broker that accesses JSONPlaceholder."
+    systemName: "AWS-S3-Bucket",
+    displayName: "AWS S3 Bucket",
+    description: "Connect to your Amazon Web Services S3 Bucket.",
+    configuration: {
+        AWSRegion: {
+            displayName: "AWS Region",
+            type: "string",
+            value: "us-west-2",
+            required: true
+        },
+        AWSBucketName: {
+            displayName: "AWS Bucket Name",
+            type: "string",
+            value: "s3-bucket-name",
+            required: true
+        },
+        AWSAccessKey: {
+            displayName: "AWS IAM User Access Key",
+            type: "string",
+            value: "IAM User Access Key",
+            required: true
+        },
+        AWSSecretKey: {
+            displayName: "AWS IAM User Secret Key",
+            type: "string",
+            value: "IAM User Secret Key",
+            required: true
+        }
+    }
 };
 
 ondescribe = async function({configuration}): Promise<void> {
     postSchema({
         objects: {
-            "todo": {
-                displayName: "TODO",
-                description: "Manages a TODO list",
+            "AWS-S3-Bucket": {
+                displayName: "AWS S3 Bucket Contents",
+                description: "Get S3 Bucket Content List of Objects",
                 properties: {
-                    "id": {
-                        displayName: "ID",
-                        type: "number"
+                    "Key": {
+                        displayName: "Key",
+                        type: "string",
                     },
-                    "userId": {
-                        displayName: "User ID",
-                        type: "number"
+                    "LastModified": {
+                        displayName: "Last Modified",
+                        type: "dateTime"
                     },
-                    "title": {
-                        displayName: "Title",
+                    "ETag": {
+                        displayName: "Etag",
                         type: "string"
                     },
-                    "completed": {
-                        displayName: "Completed",
-                        type: "boolean"
+                    "Size": {
+                        displayName: "Size",
+                        type: "number"
+                    },
+                    "StorageClass": {
+                        displayName: "Storage Class",
+                        type: "number"
                     }
                 },
                 methods: {
-                    "get": {
-                        displayName: "Get TODO",
-                        type: "read",
-                        inputs: [ "id" ],
-                        outputs: [ "id", "userId", "title", "completed" ]
-                    },
-                    "getParams": {
-                        displayName: "Get TODO",
+                    "getBucketContents": {
+                        displayName: "Get Bucket Contents",
+                        type: "list",
+                        parameters: {
+                            "prefix" : { displayName: "Prefix", description: "Directory Path", type: "string"},
+                            "max-keys" : { displayName: "Max Keys", description: "Number of Records to List. The Take.", type: "number"},
+                            "start-after" : { displayName: "Start After", description: "Key to Start After for Pagination. The Skip.", type: "string"}
+                        },
+                        outputs: [ "Key", "LastModified", "ETag", "Size", "StorageClass" ]
+                    }
+                }
+            },
+            "AWS-S3-File": {
+                displayName: "AWS S3 Bucket Contents",
+                description: "Get S3 Bucket Content List of Objects",
+                properties: {
+                    "Key": {
+                        displayName: "Key",
+                        type: "string",
+                    }
+                },
+                methods: {
+                    "create": {
+                        displayName: "Persist a file to S3",
                         type: "read",
                         parameters: {
-                            "pid" : { displayName: "param1", description: "Description Of Param 1", type: "number"} 
+                            "Key" : { displayName: "Key", description: "File Path, Name and Extension", type: "string"} 
                         },
-                        requiredParameters: [ "pid" ],
-                        outputs: [ "id" ]
+                        outputs: ["Key"]
+                    },
+                    "delete": {
+                        displayName: "Remove a file from S3",
+                        type: "delete",
+                        parameters: {
+                            "Key" : { displayName: "Key", description: "File Path, Name and Extension", type: "string"} 
+                        },
+                        outputs: ["Key"]
                     }
                 }
             }
@@ -55,21 +108,30 @@ ondescribe = async function({configuration}): Promise<void> {
 onexecute = async function({objectName, methodName, parameters, properties, configuration, schema}): Promise<void> {
     switch (objectName)
     {
-        case "todo": await onexecuteTodo(methodName, properties, parameters); break;
+        case "AWS-S3-Bucket": await onexecuteBucket(methodName, properties, parameters); break;
+        case "AWS-S3-File": await onexecuteFile(methodName, properties, parameters); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
-async function onexecuteTodo(methodName: string, properties: SingleRecord, parameters: SingleRecord): Promise<void> {
+async function onexecuteBucket(methodName: string, properties: SingleRecord, parameters: SingleRecord): Promise<void> {
     switch (methodName)
     {
-        case "get": await onexecuteTodoGet(properties); break;
-        case "getParams": await onexecuteTodoGetWithParams(parameters); break;
+        case "getBucketContents": await onexecuteGetBucketContents(properties, parameters); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
+async function onexecuteFile(methodName: string, properties: SingleRecord, parameters: SingleRecord): Promise<void> {
+    switch (methodName)
+    {
+        case "create": await onexecuteCreateFile(properties, parameters); break;
+        case "delete": await onexecuteDeleteFile(properties, parameters); break;
+        default: throw new Error("The method " + methodName + " is not supported.");
+    }
+}
+
+function onexecuteGetBucketContents(properties: SingleRecord, parameters): Promise<void> {
     return new Promise<void>((resolve, reject) =>
     {
         var xhr = new XMLHttpRequest();
@@ -80,10 +142,11 @@ function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
 
                 var obj = JSON.parse(xhr.responseText);
                 postResult({
-                    "id": obj.id,
-                    "userId": obj.userId,
-                    "title": obj.title,
-                    "completed": obj.completed
+                    "Key": obj.Key,
+                    "LastModified": obj.LastModified,
+                    "ETag": obj.ETag,
+                    "Size": obj.Size,
+                    "StorageClass": obj.StorageClass
                 });
                 resolve();
             } catch (e) {
@@ -91,24 +154,53 @@ function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
             }
         };
         
-        if(typeof properties["id"] !== "number") throw new Error("properties[\"id\"] is not of type number");
-        xhr.open("GET", 'https://jsonplaceholder.typicode.com/todos/' + encodeURIComponent(properties["id"]));
-        xhr.setRequestHeader('test', 'test value');
+        if(typeof properties["Key"] !== "string") throw new Error("properties[\"Key\"] is not of type string");
+        xhr.open("GET", 'https://' + metadata.configuration.AWSBucketName + '.s3.' + metadata.configuration.AWSRegion + '.amazonaws.com?list-type=2&max-keys=' + encodeURIComponent(parameters['max-keys']) + '&prefix=' + encodeURIComponent(parameters['prefix']) + '&start-after=' + encodeURIComponent(parameters['start=after']) + encodeURIComponent(properties["Key"]));
+        xhr.setRequestHeader('aws', 'aws s3 k2 jssp test');
         xhr.send();
     });
 }
 
-function onexecuteTodoGetWithParams(parameters: SingleRecord): Promise<void> {
+function onexecuteCreateFile(properties: SingleRecord, parameters): Promise<void> {
     return new Promise<void>((resolve, reject) =>
     {
-        try {
-            postResult({
-                "id": parameters["pid"]
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            try {
+                postResult({
+                    "Key": parameters["Key"]
+                });
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
         }
         
+        if(typeof properties["Key"] !== "string") throw new Error("properties[\"Key\"] is not of type string");
+        xhr.open("POST", 'https://' + metadata.configuration.AWSBucketName + '.s3.' + metadata.configuration.AWSRegion + '.amazonaws.com?list-type=2&max-keys=' + encodeURIComponent(parameters['max-keys']) + '&prefix=' + encodeURIComponent(parameters['prefix']) + '&start-after=' + encodeURIComponent(parameters['start=after']) + encodeURIComponent(properties["Key"]));
+        xhr.setRequestHeader('aws', 'aws s3 k2 jssp test');
+        xhr.send();
+    });
+}
+
+function onexecuteDeleteFile(properties: SingleRecord, parameters): Promise<void> {
+    return new Promise<void>((resolve, reject) =>
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            try {
+                postResult({
+                    "Key": parameters["Key"]
+                });
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
+        }
+        
+        if(typeof properties["Key"] !== "string") throw new Error("properties[\"Key\"] is not of type string");
+        xhr.open("DELETE", 'https://' + metadata.configuration.AWSBucketName + '.s3.' + metadata.configuration.AWSRegion + '.amazonaws.com?list-type=2&max-keys=' + encodeURIComponent(parameters['max-keys']) + '&prefix=' + encodeURIComponent(parameters['prefix']) + '&start-after=' + encodeURIComponent(parameters['start=after']) + encodeURIComponent(properties["Key"]));
+        xhr.setRequestHeader('aws', 'aws s3 k2 jssp test');
+        xhr.send();
     });
 }
