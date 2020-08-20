1316db4e12f6c1db98fc9c1cfab594862b76930b
Michael Trottman <mtrottman@k2.com>
2020-08-19 21:25:23 -0700

    Troubleshooting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,71 @@
 import '@k2oss/k2-broker-core';
 
 metadata = {
-    systemName: "com.k2.s3sample",
-    displayName: "Example AWS S3 Broker",
-    description: "An example broker that accesses AWS S3 Storage."
+    "systemName": "AWS-S3-Bucket",
+    "displayName": "AWS S3 Bucket",
+    "description": "Connect to your Amazon Web Services S3 Bucket.",
+    "configuration": {
+        "AWSRegion": {
+            displayName: "AWS Region",
+            type: "string",
+            required: true
+        },
+        "AWSBucketName": {
+            displayName: "AWS Bucket Name",
+            type: "string",
+            required: true
+        },
+        "AWSAccessKey": {
+            displayName: "AWS IAM User Access Key",
+            type: "string",
+            required: true
+        },
+        "AWSSecretKey": {
+            displayName: "AWS IAM User Secret Key",
+            type: "string",
+            required: true
+        }
+    }
 };
 
-ondescribe = async function(): Promise<void> {
+ondescribe = async function ({configuration}): Promise<void> {
     postSchema({
         objects: {
-            "File": {
+            "bucket": {
+                displayName: "Bucket",
+                description: "Get S3 Bucket Contents",
+                properties: {
+                    "key": {
+                        displayName: "Key",
+                        type: "string",
+                    },
+                    "lastModified": {
+                        displayName: "Last Modified",
+                        type: "dateTime"
+                    },
+                    "eTag": {
+                        displayName: "Etag",
+                        type: "string"
+                    },
+                    "size": {
+                        displayName: "Size",
+                        type: "number"
+                    },
+                    "storageClass": {
+                        displayName: "Storage Class",
+                        type: "number"
+                    }
+                },
+                methods: {
+                    "getList": {
+                        displayName: "Get List of Bucket Contents",
+                        type: "list",
+                        inputs: ["prefix", "max-keys", "start-after"],
+                        outputs: ["key", "lastModified", "eTag", "size", "storageClass"]
+                    }
+                }
+            },
+            "file": {
                 displayName: "File",
                 description: "Manages Files on AWS S3",
                 properties: {
@@ -31,7 +87,7 @@ ondescribe = async function(): Promise<void> {
                     }
                 },
                 methods: {
-                    "Get": {
+                    "getFile": {
                         displayName: "Get File",
                         type: "read",
                         inputs: [ "fileName" ],
@@ -41,25 +97,65 @@ ondescribe = async function(): Promise<void> {
                 }
             }
         }
-    });
+    }
+    )
+};
+
+onexecute = async function ({objectName, methodName, parameters, properties, configuration}): Promise<void> {
+    switch (objectName) {
+        case "bucket": await onexecuteBucket(methodName, parameters, properties, configuration); break;
+        case "file": await onexecuteFile(methodName, properties, parameters); break;
+        default: throw new Error("The object " + objectName + " is not supported.");
+    }
 }
 
-onexecute = async function(objectName, methodName, parameters, properties): Promise<void> {
-    switch (objectName)
-    {
-        case "File": await onexecuteFile(methodName, properties, parameters); break;
-        default: throw new Error("The object " + objectName + " is not supported.");
+async function onexecuteBucket(methodName: string, parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
+    switch (methodName) {
+        case "getList": await onexecuteBucketGetList(parameters, properties, configuration); break;
+        default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
 async function onexecuteFile(methodName: string, properties: SingleRecord, parameters: SingleRecord): Promise<void> {
     switch (methodName)
     {
-        case "Get": await onexecuteGetFile(parameters, properties); break;
+        case "getFile": await onexecuteGetFile(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
+function onexecuteBucketGetList(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        var urlValue = 'https://k2-jssp-bucket.s3.us-west-2.amazonaws.com?list-type=2&max-keys=50&prefix=Image&start-after=1';
+        var xhr = new XMLHttpRequest();
+        console.log('After xhr request is created');
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+
+                //console.log(xhr.responseText);
+                var obj = JSON.parse(xhr.responseText);
+                postResult(obj.map(x => {
+                    return {
+                        "key": x.key,
+                        "lastModified": x.lastModified,
+                        "etag": x.etag,
+                        "size": x.size,
+                        "storageClass": x.storageClass
+                    }
+                }));
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        xhr.open("GET", urlValue);
+        xhr.send();
+    });
+
+}
 function onexecuteGetFile(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
     return new Promise<void>((resolve, reject) =>
     {
@@ -91,7 +187,7 @@ function onexecuteGetFile(parameters: SingleRecord, properties: SingleRecord): P
 
         //xhr.setRequestHeader('test', 'test value');
         console.log("=== fn "+properties["fileName"]);
-        xhr.open("GET", 'https://rag0scnz17.execute-api.us-east-1.amazonaws.com/test/files/'+properties['fileName']);
+        xhr.open("GET", 'https://k2-jssp-bucket.s3.us-west-2.amazonaws.com/Images/IDontThinkThatMemes.jpg');
         xhr.responseType = 'blob';
 
         xhr.send();
