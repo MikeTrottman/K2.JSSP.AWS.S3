875db8e45d559263ee8a9da333e8ce6cafed07ff
Michael Trottman <mtrottman@k2.com>
2020-08-19 21:13:58 -0700

    Changing metadata object.

--- src/index.ts
+++ src/index.ts
@@ -1,252 +1,114 @@
 import '@k2oss/k2-broker-core';
 
 metadata = {
-    "systemName": "AWS-S3-Bucket",
-    "displayName": "AWS S3 Bucket",
-    "description": "Connect to your Amazon Web Services S3 Bucket."
+    systemName: "com.k2.s3sample",
+    displayName: "Example AWS S3 Broker",
+    description: "An example broker that accesses AWS S3 Storage."
 };
 
-ondescribe = async function ({configuration}): Promise<void> {
+ondescribe = async function(): Promise<void> {
     postSchema({
         objects: {
-            "bucket": {
-                displayName: "Bucket",
-                description: "Get S3 Bucket Contents",
+            "File": {
+                displayName: "File",
+                description: "Manages Files on AWS S3",
                 properties: {
-                    "key": {
-                        displayName: "Key",
-                        type: "string",
-                    },
-                    "lastModified": {
-                        displayName: "Last Modified",
-                        type: "dateTime"
-                    },
-                    "eTag": {
-                        displayName: "Etag",
+                    "fileName": {
+                        displayName: "File Name",
                         type: "string"
                     },
                     "size": {
-                        displayName: "Size",
-                        type: "number"
+                        displayName: "File Size",
+                        type: "string"
                     },
-                    "storageClass": {
-                        displayName: "Storage Class",
-                        type: "number"
+                    "contentType": {
+                        displayName: "Content Type",
+                        type: "string"
+                    },
+                    "content": {
+                        displayName: "File Content",
+                        type: "attachment"
                     }
                 },
                 methods: {
-                    "getList": {
-                        displayName: "Get List of Bucket Contents",
-                        type: "list",
-                        inputs: ["prefix", "max-keys", "start-after"],
-                        outputs: ["key", "lastModified", "eTag", "size", "storageClass"]
+                    "Get": {
+                        displayName: "Get File",
+                        type: "read",
+                        inputs: [ "fileName" ],
+                        requiredInputs: ["fileName"],
+                        outputs: [ "fileName", "size", "contentType", "content" ]
                     }
                 }
             }
         }
-    }
-    )
-};
+    });
+}
 
-onexecute = async function ({objectName, methodName, parameters, properties, configuration}): Promise<void> {
-    switch (objectName) {
-        case "posts": await onexecutePosts(methodName, parameters, properties, configuration); break;
+onexecute = async function(objectName, methodName, parameters, properties): Promise<void> {
+    switch (objectName)
+    {
+        case "File": await onexecuteFile(methodName, properties, parameters); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
-async function onexecutePosts(methodName: string, parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    switch (methodName) {
-        case "getList": await onexecutePostsGetList(parameters, properties, configuration); break;
-        case "getById": await onexecutePostsGetById(parameters, properties, configuration); break;
-        case "getByUserId": await onexecutePostsGetByUserId(parameters, properties, configuration); break;
-        case "create": await onexecutePostsCreate(parameters, properties, configuration); break;
-        case "update": await onexecutePostsUpdate(parameters, properties, configuration); break;
-        case "delete": await onexecutePostsDelete(parameters, properties, configuration); break;
+async function onexecuteFile(methodName: string, properties: SingleRecord, parameters: SingleRecord): Promise<void> {
+    switch (methodName)
+    {
+        case "Get": await onexecuteGetFile(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecutePostsGetList(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = 'https://k2-jssp-bucket.s3.us-west-2.amazonaws.com?list-type=2&max-keys=50&prefix=Image&start-after=1';
+function onexecuteGetFile(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) =>
+    {
+        console.log('===htk in onexecuteGetFile');
         var xhr = new XMLHttpRequest();
-        console.log('After xhr request is created');
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function() {
             try {
+                //console.log('=== htk readyState '+ xhr.readyState);
+                //console.log('=== htk status '+ xhr.status);
+
                 if (xhr.readyState !== 4) return;
                 if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-                //console.log(xhr.responseText);
-                var obj = JSON.parse(xhr.responseText);
-                postResult(obj.map(x => {
-                    return {
-                        "key": x.key,
-                        "lastModified": x.lastModified,
-                        "etag": x.etag,
-                        "size": x.size,
-                        "storageClass": x.storageClass
-                    }
-                }));
-                resolve();
-            } catch (e) {
-                reject(e);
-            }
-        }
+                console.log('=== hdr '+xhr.getResponseHeader('Content-Type'));
+                console.log('=== len '+xhr.getResponseHeader('Content-Length'));
+                console.log('===  file '+ xhr.response);
 
-        xhr.open("GET", urlValue);
-        xhr.send();
-    });
-
-}
-
-function onexecutePostsGetById(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = configuration["ServiceURL"] + 'posts/';
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            try {
-                if (xhr.readyState !== 4) return;
-                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
-
-                var obj = JSON.parse(xhr.responseText);
                 postResult({
-                    "id": obj.id,
-                    "userId": obj.userId,
-                    "title": obj.title,
-                    "body": obj.body
+                    "fileName": properties["fileName"],
+                    "size": xhr.getResponseHeader('Content-Length'),
+                    "contentType": xhr.getResponseHeader('Content-Type'),
+                    "content": xhr.response
                 });
                 resolve();
             } catch (e) {
                 reject(e);
             }
-        }
+        };
 
-        if(typeof properties["id"] !== "number") throw new Error("properties[\"id\"] is not of type number");
+        //xhr.setRequestHeader('test', 'test value');
+        console.log("=== fn "+properties["fileName"]);
+        xhr.open("GET", 'https://rag0scnz17.execute-api.us-east-1.amazonaws.com/test/files/'+properties['fileName']);
+        xhr.responseType = 'blob';
 
-        xhr.open("GET", urlValue + encodeURIComponent(properties["id"]));
         xhr.send();
     });
 }
 
-function onexecutePostsGetByUserId(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = configuration["ServiceURL"] + 'posts?userId=';
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            try {
-                if (xhr.readyState !== 4) return;
-                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
-
-                var obj = JSON.parse(xhr.responseText);
-                postResult(obj.map(x => {
-                    return {
-                        "id": x.id,
-                        "userId": x.userId,
-                        "title": x.title,
-                        "body": x.body
-                    }
-                }));
-                resolve();
-            } catch (e) {
-                reject(e);
-            }
-
+function onexecuteTodoGetWithParams(parameters: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) =>
+    {
+        try {
+            postResult({
+                "id": parameters["pid"]
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
         }
-
-        if(typeof properties["userId"] !== "number") throw new Error("properties[\"userId\"] is not of type number");
-        xhr.open("GET", urlValue + encodeURIComponent(properties["userId"]));
-        xhr.send();
-    });
-}
-
-
-function onexecutePostsCreate(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = configuration["ServiceURL"] + 'posts/';
-        var data = JSON.stringify({
-            "userId": properties["userId"],
-            "title": properties["title"],
-            "body": properties["body"]
-        });
-
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            try {
-                if (xhr.readyState !== 4) return;
-                // look for 'created' code
-                if (xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
-
-                var obj = JSON.parse(xhr.responseText);
-                postResult({
-                    "id": obj.id,
-                    "userId": obj.userId,
-                    "title": obj.title,
-                    "body": obj.body
-                });
-                resolve();
-            } catch (e) {
-                reject(e);
-            }
-        }
-        xhr.open("POST", urlValue);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(data);
-    });
-}
-
-function onexecutePostsUpdate(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = configuration["ServiceURL"] + 'posts/';
-        var data = JSON.stringify({
-            "userId": properties["userId"],
-            "title": properties["title"],
-            "body": properties["body"]
-        });
-
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            try {
-                if (xhr.readyState !== 4) return;
-                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
-
-                var obj = JSON.parse(xhr.responseText);
-                postResult({
-                    "id": obj.id,
-                    "userId": obj.userId,
-                    "title": obj.title,
-                    "body": obj.body
-                });
-                resolve();
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        if(typeof properties["id"] !== "number") throw new Error("properties[\"id\"] is not of type number");
-        xhr.open("PUT", urlValue + encodeURIComponent(properties["id"]));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(data);
-    });
-}
-
-function onexecutePostsDelete(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        var urlValue = configuration["ServiceURL"];
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            try {
-                if (xhr.readyState !== 4) return;
-                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
-                resolve();
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        if(typeof properties["id"] !== "number") throw new Error("properties[\"id\"] is not of type number");
-        xhr.open("DELETE", urlValue + 'posts/' + encodeURIComponent(properties["id"]));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send();
+        
     });
 }
